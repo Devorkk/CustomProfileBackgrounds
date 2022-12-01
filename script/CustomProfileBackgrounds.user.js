c6991e373063959a4b7b69e2749143fd59ceb1a6
Devork <88288729+Devorkk@users.noreply.github.com>
2022-12-01 20:18:30 +0100

    Create CustomProfileBackgrounds.user.js

---
+++ script/CustomProfileBackgrounds.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Custom Profile Backgrounds
+// @version      1.0
+// @author       Devork
+// @namespace    https://github.com/Devorkk/
+// @match        https://www.kogama.com/profile/*
+// @require      https://raw.githubusercontent.com/Devorkk/CustomProfileBackgrounds/main/script/CustomProfileBackgrounds.js
+// @run-at 		   document-start
+// @grant        none
+// ==/UserScript==
