f9820a93a80be55ad83bf5f478d901157e013d0c
Devork <88288729+Devorkk@users.noreply.github.com>
2022-12-02 17:31:57 +0000

    add case-sensitive

--- script/CustomProfileBackgrounds.js
+++ script/CustomProfileBackgrounds.js
@@ -3,7 +3,7 @@ const InsertBeforeLoad = async () => {
 	const BACKGROUND_AVATAR = document.querySelector('.background-avatar')
 	const SECTION_BACKGROUND = document.querySelector('.section-top-background')
 
-	const INDEX_OF_BACKGROUND = TEXT_DESCRIPTION.indexOf('Background:')
+	const INDEX_OF_BACKGROUND = TEXT_DESCRIPTION.toLowerCase().indexOf('Background:'.toLowerCase())
 	const INDEX_OF_FILTER = TEXT_DESCRIPTION.indexOf('filter:')
 
 	if (INDEX_OF_BACKGROUND != -1 && INDEX_OF_FILTER != -1) {
@@ -11,8 +11,8 @@ const InsertBeforeLoad = async () => {
 		const TEXT = TEXT_DESCRIPTION.slice(INDEX_OF_BACKGROUND, INDEX_OF_FILTER + 13).split(' ')
 		const GAME_ID = TEXT[1].replace(',', '')
 		const FILTER = TEXT[3].replace(';', '')
-		const IMAGE = (await(await fetch(`/game/published/?q=${GAME_ID}`)).json()).images.large.replace('cache', 'images').replace('_600x240.jpg', '.png')
-		
+		const IMAGE = (await (await fetch(`/game/published/?q=${GAME_ID}`)).json()).images.large.replace('cache', 'images').replace('_600x240.jpg', '.png')
+
 		BACKGROUND_AVATAR.style.backgroundImage = `url(${IMAGE})`;
 
 		if (FILTER == 'blur') {
